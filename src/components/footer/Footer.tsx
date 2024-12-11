8b8bfa4716fea81c083ee2d0ae9bfc73952712f7
Olga Mahin <olgamahin@hotmail.com>
2024-12-11 16:54:44 -0300

    :tada: Adding project

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledFooter } from './styles'
+
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      Desenvolvido com 🤍 por Olga Mahin!
+    </StyledFooter>
+  );
+};
+
+export default Footer;
